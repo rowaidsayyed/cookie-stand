c811db1028fa2214a1eef5ef8ca36a22a385c35e
rowidsayyed <rowidsayyed@gmail.com>
2020-02-26 01:12:50 +0200

    update CSS

--- JS/app.js
+++ JS/app.js
@@ -2,8 +2,7 @@
 
 //Global variables
 var workingTime =  ['06am','07am','08am','09am','10am','11am','12pm','01pm','02pm','03pm','04pm','05pm','06pm','07pm'];
-var wrokinghours = workingTime.length;
-var loca =[];
+var loca = [];
 
 
 // Branch object using construction notation
@@ -20,13 +19,13 @@ Branch.prototype.randCus = function(){
   this.numCus = randomCus(this.minCus,this.maxCus);
 };
 Branch.prototype.purchaseCookies = function(){
-  var cookiesPurchasedEqu = Math.ceil(this.avgCookie * this.numCus);
+  var cookiesPurchasedEqu = Math.floor(this.avgCookie * this.numCus);
   this.cookiesPurchased.push(cookiesPurchasedEqu);
   this.total += cookiesPurchasedEqu;
 };
 
 Branch.prototype.render = function(){
-  for(var i =0; i<wrokinghours;i++ ){
+  for(var i =0; i<workingTime.length;i++ ){
     this.randCus();
     this.purchaseCookies();
   }loca.push(this);
@@ -49,21 +48,19 @@ paris.render();
 lima.render();
 //amman.render();
 
-//var loca = [seattle,tokyo,dubai,paris,lima,amman];
-var mainEl = document.getElementById('main');
-var divEl = document.createElement('div');
-mainEl.appendChild(divEl);
+
+var mainEl = document.getElementById('tableDiv');
 var table =document.createElement('table');
 mainEl.appendChild(table);
 
-//Display Data on the Table
+//Display Data to the Table
 function headerhour(){
-  var rowTable =document.createElement('tr');
-  table.appendChild(rowTable);
   var headerTable = document.createElement('thead');
-  rowTable.appendChild(headerTable);
+  var rowTable =document.createElement('tr');
+  table.appendChild(headerTable);
+  headerTable.appendChild(rowTable);
   var headTable =document.createElement('th');
-  headerTable.appendChild(headTable);
+  rowTable.appendChild(headTable);
   headTable.textContent = '';
   for(var i=0;i<workingTime.length;i++){
     headTable =document.createElement('th');
@@ -79,13 +76,17 @@ function headerhour(){
 function bodycont(){
   var totalLocation = 0;
   var rowTable =document.createElement('tr');
-  table.appendChild(rowTable);
+  var bodyTable = document.createElement('tbody');
+  bodyTable.appendChild(rowTable);
+  table.appendChild(bodyTable);
   for(var q=0;q<loca.length;q++){
     rowTable =document.createElement('tr');
-    table.appendChild(rowTable);
-    rowTable.textContent = loca[q].location;
+    bodyTable.appendChild(rowTable);
+    var dataTable =document.createElement('td');
+    rowTable.appendChild(dataTable);
+    dataTable.textContent = loca[q].location;
     for(var k=0;k<workingTime.length;k++){
-      var dataTable =document.createElement('td');
+      dataTable =document.createElement('td');
       rowTable.appendChild(dataTable);
       dataTable.textContent = loca[q].cookiesPurchased[k];
     }
@@ -102,12 +103,14 @@ function footerTable(){
   var totalAll = 0 ;
   var rowTable =document.createElement('tr');
   table.appendChild(rowTable);
-  rowTable.textContent = 'Total';
+  var dataTable =document.createElement('td');
+  rowTable.appendChild(dataTable);
+  dataTable.textContent = 'Total';
   for(var q=0;q<workingTime.length;q++){
     for(var k=0;k<loca.length;k++){
       totalHour += loca[k].cookiesPurchased[q];
     }
-    var dataTable =document.createElement('td');
+    dataTable =document.createElement('td');
     rowTable.appendChild(dataTable);
     dataTable.textContent = totalHour;
     totalAll = totalHour + totalAll;
@@ -118,14 +121,28 @@ function footerTable(){
   dataTable.textContent = totalAll;
 }
 
-
-
 headerhour();
 bodycont();
 footerTable();
 
+//map(); // to show names of the branches below the map at index.html page
 
 
 function randomCus(min,max){
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
+
+function map(){
+  var mapEl = document.getElementsByClassName('classmap');
+  console.log(mapEl);
+  for(var y=0; y<loca.length;y++){
+    var liEl = document.createElement('li');
+    liEl.className = 'noli';
+    console.log(liEl);
+    liEl.textContent = loca[y].location;
+    mapEl.appendChild(liEl);
+  }
+}
+
+
+
